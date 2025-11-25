c99d2ca3d75dc3e55401394612f32567c64b5567
Sachin <SachinMourya303@gmail.com>
2025-11-25 19:36:56 +0530

    Updated the post seller auth request ui and logic

---
+++ client/src/app/utils/sellerForm.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { setUsers } from '../state-management/slices/userData';
+import toast from 'react-hot-toast';
+
+export const sellerSignupRequest = async (dispatch, setSellerData, companyname, sellername, email, phone, address, city, state, country, pincode, password) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_SERVER_URI}/seller/signup`, { companyname, sellername, email, phone, address, city, state, country, pincode, password });
+        dispatch(setUsers(response.data));
+        toast.success(response.data.message);
+        setSellerData({
+            companyname: "",
+            sellername: "",
+            email: "",
+            phone: "",
+            address: "",
+            city: "",
+            state: "",
+            country: "",
+            pincode: "",
+            password: "",
+        });
+    } catch (error) {
+        toast.error(error.message);
+    }
+}
+
+export const sellerSigninRequest = async (email, password) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_SERVER_URI}/seller/signin`, { email, password });
+        toast.success(response.data.message);
+    } catch (error) {
+        toast.error(error.message);
+    }
+}
