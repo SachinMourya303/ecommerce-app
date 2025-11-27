751c0233ae322b8751b1b6dffb484eb1a601accb
Sachin <SachinMourya303@gmail.com>
2025-11-27 13:20:18 +0530

    Updated the seller product post request

--- client/src/app/components/Product_details.jsx
+++ client/src/app/components/Product_details.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { IndianRupee, LayoutGrid, MapPin, Package, Pill, Star, Store } from 'lucide-react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { IndianRupee, LayoutGrid, Loader, MapPin, Package, Pill, Star, Store } from 'lucide-react'
 
-const Product_details = () => {
+const Product_details = ({ productForm, productData, setProductData }) => {
     const darkmode = useSelector(state => state.userData.darkmode);
+    const loader = useSelector(state => state.userData.loader);
 
     const productDetails = [
-        { title: "Product Name", icon: Package },
-        { title: "Product Price", icon: IndianRupee },
-        { title: "Sold By", icon: Store },
-        { title: "Company Address", icon: MapPin },
+        { id: "name", title: "Product Name", icon: Package },
+        { id: "price", title: "Product Price", icon: IndianRupee },
+        { id: "sold_by", title: "Sold By", icon: Store },
+        { id: "address", title: "Company Address", icon: MapPin },
     ]
 
     const dropdown = [
         {
+            id: "category",
             title: "Choose Category",
             icon: LayoutGrid,
             option: [
@@ -35,6 +37,7 @@ const Product_details = () => {
             ]
         },
         {
+            id: "rating",
             title: "Choose Rating",
             icon: Star,
             option: [
@@ -48,25 +51,47 @@ const Product_details = () => {
     ]
 
     const productColor = [
-        { title: "Black", value: "Black" },
-        { title: "Red", value: "Red" },
-        { title: "Blue", value: "Blue" },
-        { title: "Green", value: "Green" },
-        { title: "Yellow", value: "Yellow" },
-        { title: "Violet", value: "Violet" },
-        { title: "Purple", value: "Purple" },
-        { title: "White", value: "White" },
-        { title: "Pink", value: "Pink" },
-        { title: "Brown", value: "Brown" },
+        { id: "color", title: "Black", value: "Black" },
+        { id: "color", title: "Red", value: "Red" },
+        { id: "color", title: "Blue", value: "Blue" },
+        { id: "color", title: "Green", value: "Green" },
+        { id: "color", title: "Yellow", value: "Yellow" },
+        { id: "color", title: "Violet", value: "Violet" },
+        { id: "color", title: "Purple", value: "Purple" },
+        { id: "color", title: "White", value: "White" },
+        { id: "color", title: "Pink", value: "Pink" },
+        { id: "color", title: "Brown", value: "Brown" },
     ]
+
+    const onInputChange = (e) => {
+        const { name, value, type, checked } = e.target;
+
+        if (type === "checkbox") {
+            setProductData(prev => {
+                let newColors = [...prev.color];
+
+                if (checked) {
+                    newColors.push(value);
+                } else {
+                    newColors = newColors.filter(item => item !== value);
+                }
+
+                return { ...prev, color: newColors };
+            });
+        }
+        else {
+            setProductData(prev => ({ ...prev, [name]: value }));
+        }
+    }
+
     return (
-        <form className='w-full flex flex-col md:w-[80%] mt-10'>
+        <form onSubmit={productForm} className='w-full flex flex-col md:w-[80%] mt-10'>
             <div className='flex max-md:flex-col md:flex-wrap justify-between'>
                 {
                     productDetails.map((data, index) => (
                         <div key={index} className={`w-full md:w-[49%] flex items-center gap-2 my-3 border border-amber-700 border-b-5 ${darkmode ? 'hover:bg-rose-900 text-rose-600 ' : 'hover:bg-rose-100 text-rose-900 '} transition-all active:scale-95 rounded-lg font-medium cursor-pointer px-2`}>
                             < data.icon className='size-4' />
-                            <input required placeholder={data.title} className=' outline-none w-full py-2.5' />
+                            <input onChange={onInputChange} value={productData[data.id]} name={data.id} required placeholder={data.title} className=' outline-none w-full py-2.5' />
                         </div>
                     ))
                 }
@@ -74,7 +99,7 @@ const Product_details = () => {
 
             <div className={`w-full flex gap-2 my-3 border border-amber-700 border-b-5 py-2.5 ${darkmode ? 'hover:bg-rose-900 text-rose-600 ' : 'hover:bg-rose-100 text-rose-900 '} transition-all active:scale-95 rounded-lg font-medium cursor-pointer px-2`}>
                 < Pill className='size-4 mt-1' />
-                <textarea required  placeholder="Product Description" className=' outline-none w-full' />
+                <textarea onChange={onInputChange} value={productData.description} name='description' required placeholder="Product Description" className=' outline-none w-full' />
             </div>
 
             <div className='flex max-md:flex-col md:flex-wrap justify-between'>
@@ -82,7 +107,7 @@ const Product_details = () => {
                     dropdown.map((data, index) => (
                         <div key={index} className={`w-full md:w-[49%] flex items-center gap-2 my-3 border border-amber-700 border-b-5 ${darkmode ? 'hover:bg-rose-900 text-rose-600 ' : 'hover:bg-rose-100 text-rose-900 '} transition-all active:scale-95 rounded-lg font-medium cursor-pointer px-2`}>
                             <data.icon className='size-4' />
-                            <select name="" id="" className='w-full py-2.5 outline-none text-rose-900/50' required>
+                            <select onChange={onInputChange} value={productData[data.id]} name={data.id} className='w-full py-2.5 outline-none text-rose-900/50' required>
                                 <option >{data.title}</option>
                                 {
                                     data.option.map((item, index) => (
@@ -98,18 +123,27 @@ const Product_details = () => {
             <div>
                 <p className='mt-5'>Choose Product Color</p>
                 <div className='flex flex-wrap items-center gap-5'>
-                    {
-                    productColor.map((color, index) => (
-                        <div className='flex gap-2 mt-2'>
-                            <input id={color.title} value={color.value} type="checkbox" />
-                            <label htmlFor={color.title} className='text-rose-900 text-xs'>{color.title}</label>
+                    {productColor.map((color, index) => (
+                        <div key={index} className='flex gap-2 mt-2'>
+                            <input
+                                type="checkbox"
+                                name="color"
+                                value={color.title}
+                                checked={productData.color.includes(color.title)}
+                                onChange={onInputChange}
+                            />
+                            <label htmlFor={color.title} className='text-rose-900 text-xs'>
+                                {color.title}
+                            </label>
                         </div>
-                    ))
-                }
+                    ))}
                 </div>
             </div>
 
-            <button type='submit' className={`w-[50%] md:w-[30%] flex items-center justify-center my-3 bg-amber-500 mt-10 ${darkmode ? 'hover:bg-rose-900 text-rose-600 ' : 'hover:bg-rose-100 text-rose-900 '} transition-all active:scale-95 py-2.5 rounded-lg font-medium cursor-pointer`}>Submit</button>
+
+            <button type='submit' className={`w-[50%] md:w-[30%] flex items-center justify-center my-3 bg-amber-500 mt-10 ${darkmode ? 'hover:bg-rose-900 text-rose-600 ' : 'hover:bg-rose-100 text-rose-900 '} transition-all active:scale-95 py-2.5 rounded-lg font-medium cursor-pointer`}>
+                {loader ? <Loader className='animate-spin' /> : 'Submit'}
+            </button>
         </form>
     )
 }
