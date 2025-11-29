b4550306fa4c75b7b92fcb7e311bc7d61ea29c3d
Sachin <SachinMourya303@gmail.com>
2025-11-29 17:08:58 +0530

    Updated the customer routes

--- server/app/routes/customerRoutes.js
+++ server/app/routes/customerRoutes.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import { signInValidation, signUpValidation } from '../middleware/customerValidation.js';
 import { signIn, signUp } from '../controllers/customerController.js';
+import customerModel from '../model/customerModel.js';
 
 const customerRoutes = express.Router();
 
 customerRoutes.post('/signup', signUpValidation, signUp);
 customerRoutes.post('/signin', signInValidation, signIn);
+
+customerRoutes.get('/accounts', async (req, res) => {
+    try {
+        const allCustomers = await customerModel.find({});
+        return res.status(200).json(allCustomers);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+})
 
 export default customerRoutes;
