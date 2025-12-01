68a50e9d033471f3904313fadd241edf74f3f0b0
Sachin <SachinMourya303@gmail.com>
2025-12-01 18:50:49 +0530

    Updated the cart model and routes

--- server/app/model/cartModel.js
+++ server/app/model/cartModel.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const cartSchema = new mongoose.Schema({
     product_image : { type: String , required: true },
     product_name: { type: String , required: true },
-    size: { type: String, required: true },
     price: { type: String, required: true },
     quantity: { type: Number, default: 1 },
 }, {
